565db89fb4b6c4d8ffee3d350b9bcd632fcea802
mar69287 <marcoa.ruiz97@gmail.com>
2023-09-26 11:41:11 -0700

    new component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import { Box } from "@chakra-ui/react"
+
+const Contact = () => {
+  return (
+    <Box
+        h="100vh"
+        bgColor={"blue.600"}
+        scrollSnapAlign={'center'}
+        scrollBehavior={'smooth'}
+    >
+
+    </Box>
+  )
+}
+
+export default Contact
