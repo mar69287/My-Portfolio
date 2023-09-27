81022a4a7562e78141686124c1190466d137472c
mar69287 <marcoa.ruiz97@gmail.com>
2023-09-26 21:08:30 -0700

    add skill logos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,27 @@
 import { Box, Grid, GridItem, HStack, Image, VStack, Link, Text, useBreakpointValue, Heading } from "@chakra-ui/react";
 import profile from "../assets/portfolio.png"
 import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai'
-import { BsCodeSlash } from 'react-icons/bs'
+// import { BsCodeSlash } from 'react-icons/bs'
+import { SiChakraui, SiBootstrap, SiExpress, SiNodedotjs, SiPython, SiPostgresql, SiMongodb } from 'react-icons/si'
+import { BiLogoReact, BiLogoJavascript, BiLogoTypescript } from 'react-icons/bi'
 import resume from '../assets/Marco-Software-Resume.pdf'
+import materialUI from '../assets/material-ui.png'
+import cLogo from '../assets/c++.png'
 
 const About = () => {
   const isLargeScreen = useBreakpointValue({ base: false, lg: true });
-  const hexagonStyles = {
-    width: "40px",
-    height: "40px",
-    backgroundColor: "rgb(1, 188, 193)",
-    color: "#fff",
-    fontSize: '1.8rem',
-    position: "relative",
-    clipPath: "polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  };
+  // const hexagonStyles = {
+  //   width: "40px",
+  //   height: "40px",
+  //   backgroundColor: "rgb(1, 188, 193)",
+  //   color: "#fff",
+  //   fontSize: '1.8rem',
+  //   position: "relative",
+  //   clipPath: "polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%)",
+  //   display: "flex",
+  //   justifyContent: "center",
+  //   alignItems: "center",
+  // };
 
   return (
     <Box minH="100vh" bgColor={"#fff"} id="about" width={{xl: '100%', '2xl': '1400px'}} m='0 auto'>
@@ -107,28 +111,105 @@ const About = () => {
                 </Text>
             </VStack> 
         </GridItem>
-        <GridItem colSpan={{ base: 1, lg: 6 }} mt={5}>
-          <HStack w={'14rem'} justifyContent={'space-between'} gap={4} alignItems={'center'}  borderRight={'2px solid #ddd'} pr={3}>
+        <GridItem colSpan={{ base: 1, lg: 6 }} mt={5} borderBottom={'2px solid #ddd'} >
+          <HStack>
+            <HStack w={'11rem'} justifyContent={'space-between'} gap={4} alignItems={'center'} pr={3}>
               <Heading color={'rgb(33, 36, 46)'} size={'lg'}>
                   Front End
               </Heading>
-              <Box style={hexagonStyles}>
+              {/* <Box style={hexagonStyles}>
                 <BsCodeSlash />
-              </Box>
+              </Box> */}
             </HStack>
-            <HStack>
-              
+            <HStack flex={1} justifyContent={'space-evenly'} borderLeft={'2px solid #ddd'} py={3}>
+              <VStack gap={0}>
+                <Box fontSize={'2rem'} color={'rgb(1, 188, 193)'}>
+                  <BiLogoReact />
+                </Box>
+                <Text fontSize={'xl'}>React</Text>
+              </VStack>
+              <VStack gap={0}>
+                <Box fontSize={'2rem'} color={'rgb(232, 208, 70)'}>
+                  <BiLogoJavascript />
+                </Box>
+                <Text fontSize={'xl'}>JavaScript</Text>
+              </VStack>
+              <VStack gap={0}>
+                <Box fontSize={'2rem'} color={'rgb(30, 105, 184)'}>
+                  <BiLogoTypescript />
+                </Box>
+                <Text fontSize={'xl'}>TypeScript</Text>
+              </VStack>
+              <VStack gap={0}>
+                <Box fontSize={'1.9rem'} color={'rgb(1, 188, 193)'}>
+                  <SiChakraui />
+                </Box>
+                <Text fontSize={'xl'}>Chakra-UI</Text>
+              </VStack>
+              <VStack gap={0}>
+                <Box boxSize='30px'>
+                  <Image src={materialUI} alt='Dan Abramov' />
+                </Box>
+                <Text fontSize={'xl'}>Material-UI</Text>
+              </VStack>
+              <VStack gap={0}>
+                <Box fontSize={'1.9rem'} color={'rgb(99, 23, 241)'}>
+                  <SiBootstrap />
+                </Box>
+                <Text fontSize={'xl'}>Chakra-UI</Text>
+              </VStack>
             </HStack>
+          </HStack>
         </GridItem>
-        <GridItem colSpan={{ base: 1, lg: 6 }} mt={5}>
-          <HStack w={'14rem'} justifyContent={'space-between'} gap={4} alignItems={'center'}  borderRight={'2px solid #ddd'} pr={3}>
+        <GridItem colSpan={{ base: 1, lg: 6 }} >
+          <HStack>
+            <HStack w={'11rem'} justifyContent={'space-between'} gap={4} alignItems={'center'} pr={3}>
               <Heading color={'rgb(33, 36, 46)'} size={'lg'}>
                   Back End
               </Heading>
-              <Box style={hexagonStyles}>
+              {/* <Box style={hexagonStyles}>
                 <BsCodeSlash />
-              </Box>
+              </Box> */}
             </HStack>
+            <HStack flex={1} justifyContent={'space-evenly'} borderLeft={'2px solid #ddd'} py={3}>
+              <VStack gap={0}>
+                <Box fontSize={'2rem'} color={'black'}>
+                  <SiExpress />
+                </Box>
+                <Text fontSize={'xl'}>Express</Text>
+              </VStack>
+              <VStack gap={0}>
+                <Box fontSize={'2rem'} color={'rgb(49, 125, 56)'}>
+                  <SiNodedotjs />
+                </Box>
+                <Text fontSize={'xl'}>Node</Text>
+              </VStack>
+              <VStack gap={0}>
+                <Box fontSize={'2rem'} color={'rgb(30, 105, 184)'}>
+                  <SiPython />
+                </Box>
+                <Text fontSize={'xl'}>Python</Text>
+              </VStack>
+              <VStack gap={0}>
+                <Box boxSize='33px'>
+                  <Image src={cLogo} alt='Dan Abramov' />
+                </Box>
+                <Text fontSize={'xl'}>C++</Text>
+              </VStack>
+              <VStack gap={0}>
+                <Box fontSize={'1.9rem'} color={'rgb(30, 105, 184)'}>
+                  <SiPostgresql />
+                </Box>
+                <Text fontSize={'xl'}>PostgreSQL</Text>
+              </VStack>
+              <VStack gap={0}>
+                <Box fontSize={'1.9rem'} color={'rgb(49, 125, 56)'}>
+                  <SiMongodb />
+                </Box>
+                <Text fontSize={'xl'}>MongoDB</Text>
+              </VStack>
+            </HStack>
+          </HStack>
         </GridItem>
         {/* <GridItem colSpan={{ base: 1, lg: 6 }} borderBottom={'2px solid #ddd'}>
           <Heading color={'rgb(33, 36, 46)'} size={'3xl'} mt={20} mb={9} >
