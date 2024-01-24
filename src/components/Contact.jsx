6c6ffb7c02af46d8fb3725d245c0f37b78a07816
mar69287 <marcoa.ruiz97@gmail.com>
2024-01-24 15:39:33 -0800

    redesign contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,11 @@
-import { Box, Button, FormControl, Grid, GridItem, Heading, Input, Text, Textarea, VStack, Link, HStack } from "@chakra-ui/react";
-import { AiFillGithub, AiFillLinkedin, AiOutlineMail } from 'react-icons/ai'
-import { MdArrowForward } from 'react-icons/md';
+import { Box, Button, FormControl, Input, Text, Textarea, VStack, Link, HStack } from "@chakra-ui/react";
+import { AiOutlineMail, AiOutlineDownload } from 'react-icons/ai'
 import resume from '../assets/Marco-Software-Resume.pdf'
 import emailjs from 'emailjs-com';
 import { useRef, useState } from "react";
+import Headers from "./Headers";
+import { FiGithub } from "react-icons/fi";
+import { FaLinkedinIn } from "react-icons/fa";
 
 const Contact = () => {
   const form = useRef();
@@ -36,101 +38,123 @@ const Contact = () => {
  
   return (
     <Box
-        bgColor={"#252934"}
         id="contact"
         as="section"
-        py={20}
-        px={{base: '5', lg: '0'}}
+        pt={{base:20, lg: 24}}
+        position={'relative'}
+        minH={'100vh'}
+        width={{xl: '100%', '2xl': '1000px'}} mx={'auto'}
     >
-      <Grid
-        templateColumns={{ base: "1fr", md: "1fr 2fr" }}
-        width={{xl: '100%', '2xl': '1400px'}} m='0 auto'
-        px={{base: 0, lg: 10}}
-        color={'#fff'}
-        gap={10}
+      <Headers left={'contact'} right={'me'} />
+      <VStack 
+        alignItems={'center'}
+        px={5}
+        pb={5}
+        pt={{base: 5, lg: 0}}
       >
-        <GridItem colSpan={1} h="100%">
-          <VStack w={'100%'} justifyContent={'center'} alignItems={{base: 'center', md: 'flex-start'}} gap={10}>
-            <Heading size={{base: 'xl', lg: '2xl'}} textAlign={{base: 'center', md: 'left'}}>
-              Locating me is a breeze, let&rsquo;s get in touch!
-            </Heading>
-            <Box w={'33%'} border={'1px solid #fff'}></Box>
-            <HStack gap={10} justifyContent={{base: 'center', md: 'flex-start'}} alignItems={'center'} w={'100%'}>
-              <Link 
-                href="https://github.com/mar69287" 
-                isExternal 
-                // color={'#616161'} 
-                fontSize={'2.2rem'}
-                _hover={{
-                  color: "#00989D", 
-                  transition: "color 0.3s ease",
-                }}
-              >
-                <AiFillGithub />
-              </Link>
-              <Link 
-                href="https://www.linkedin.com/in/marcoruizjr/" 
-                isExternal 
-                // color={'#616161'} 
-                fontSize={'2.2rem'}
-                _hover={{
-                  color: "#00989D", 
-                  transition: "color 0.3s ease", 
-                }}
-              >
-                <AiFillLinkedin />
-              </Link>
-              <Link 
-                href="mailto:marcoa.ruiz97@gmail.com" 
-                isExternal 
-                // color={'#616161'} 
-                fontSize={'2.2rem'}
-                _hover={{
-                  color: "#00989D", 
-                  transition: "color 0.3s ease", 
-                }}
-              >
-                <AiOutlineMail />
-              </Link>
-            </HStack>
-          </VStack>
-            <Link isExternal href={resume} _hover={{ textDecoration: 'none' }}>
-                <HStack mt={6} className='btn'>
-                    <Text fontSize={'2xl'} fontWeight={'semibold'}>Resume</Text>
-                    <MdArrowForward className="arrow-icon"/>
-                </HStack>
-            </Link>
-        </GridItem>
-        <form ref={form} onSubmit={sendEmail}>
-          <VStack w={'100%'}  h="100%" alignItems={'flex-end'} justifyContent={'center'}>
-            <FormControl>
-              <Input type="text" value={name} onChange={(e) => setName(e.target.value)} borderRadius={'none'} borderBottom={'2px solid #fff'} borderTop={'none'} borderRight={'none'} borderLeft={'none'}  placeholder="Your Name" _placeholder={{ color: '#b9b9b9' }}/>
-            </FormControl>
+        <Text fontSize={{base: 'lg', lg: 'xl'}} lineHeight={.8}>
+          Locating me is a breeze!
+        </Text>
+        <Box w={{base: '40%'}} border={'1px solid black'} my={6}/>
+        <HStack justifyContent={'center'} alignItems={'center'} gap={{base: 7}}>
+          <Link 
+            href="https://github.com/mar69287" 
+            isExternal 
+            color={'#fff'} 
+            fontSize={{base: '1rem', lg: '1.2rem'}}
+            borderRadius={'full'}
+            bg={'rgba(97, 97, 97, 0.9)'}
+            border={'1px solid RGBA(0, 0, 0, 0.64)'}
+            p={2}
+            _hover={{
+              color: "#e31b60", 
+              transition: "color 0.3s ease",
+            }}
+          >
+            <FiGithub />
+          </Link>
+          <Link 
+            href="https://www.linkedin.com/in/marcoruizjr/" 
+            isExternal 
+            color={'#fff'} 
+            fontSize={{base: '1rem', lg: '1.2rem'}}
+            borderRadius={'full'}
+            bg={'rgba(97, 97, 97, 0.9)'}
+            border={'1px solid RGBA(0, 0, 0, 0.64)'}
+            p={2}
+            _hover={{
+              color: "#e31b60", 
+              transition: "color 0.3s ease",
+            }}
+          >
+            <FaLinkedinIn />
+          </Link>
+          <Link 
+            href="mailto:marcoa.ruiz97@gmail.com" 
+            isExternal 
+            color={'#fff'} 
+            fontSize={{base: '1rem', lg: '1.2rem'}}
+            borderRadius={'full'}
+            bg={'rgba(97, 97, 97, 0.9)'}
+            border={'1px solid RGBA(0, 0, 0, 0.64)'}
+            p={2}
+            _hover={{
+              color: "#e31b60", 
+              transition: "color 0.3s ease",
+            }}
+          >
+            <AiOutlineMail />
+          </Link>
+          <Link 
+            href={resume} 
+            isExternal 
+            color={'#fff'} 
+            fontSize={{base: '1rem', lg: '1.2rem'}}
+            borderRadius={'full'}
+            bg={'rgba(97, 97, 97, 0.9)'}
+            border={'1px solid RGBA(0, 0, 0, 0.64)'}
+            p={2}
+            _hover={{
+              color: "#e31b60", 
+              transition: "color 0.3s ease",
+            }}
+          >
+            <AiOutlineDownload />
+          </Link>
+        </HStack>
+        <FormControl
+          as="form"
+          name="contactForm"
+          method="post" 
+          action="#"
+          width={{base: '100%', md: '700px'}} ref={form} onSubmit={sendEmail}
+          mt={{base:5}}
+        >
+          <VStack  alignItems={'center'} justifyContent={'center'} gap={6}>
+              <Input type="text" value={name} onChange={(e) => setName(e.target.value)} borderRadius={'none'} borderBottom={'2px solid #000'} borderTop={'none'} borderRight={'none'} borderLeft={'none'}  placeholder="Your Name" _placeholder={{ color: '#000' }}/>
 
-            <FormControl mt={6}>
-              <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Your Email" _placeholder={{ color: '#b9b9b9' }} borderRadius={'none'} borderBottom={'2px solid #fff'} borderTop={'none'} borderRight={'none'} borderLeft={'none'}/>
-            </FormControl>
+              <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Your Email" _placeholder={{ color: '#000' }} borderRadius={'none'} borderBottom={'2px solid #000'} borderTop={'none'} borderRight={'none'} borderLeft={'none'}/>
 
-            <FormControl mt={8}>
-              <Textarea placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)} _placeholder={{ color: '#b9b9b9' }} borderRadius={'none'} borderBottom={'2px solid #fff'} borderTop={'none'} borderRight={'none'} borderLeft={'none'}/>
-            </FormControl>
+              <Textarea mt={4} placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)} _placeholder={{ color: '#000' }} borderRadius={'none'} borderBottom={'2px solid #000'} borderTop={'none'} borderRight={'none'} borderLeft={'none'}/>
 
             <Button
               mt={4}
-              bg={'#252934'}
-              color={'#fff'}
-              borderRadius={0}
-              _hover={{ bg: "#00989D", border: "2px solid #00989D", transition: "all .5s ease", }}
-              border={'2px solid #fff'}
+              bg={'#fff'}
+              color={'#000'}
+              borderRadius={6}
+              _hover={{ bg: "#e31b60", color: 'white', border: "2px solid #000", transition: "all .5s ease", }}
+              border={'2px solid #000'}
               px={'1.6rem'}
               py={'1.2rem'}
               type="submit"
+              w={'max-content'}
             >
               SUBMIT
             </Button>
           </VStack>
-        </form>
-      </Grid>
+        </FormControl>
+      </VStack>
+      
     </Box>
   )
 }
