5538813275d9c020fb1acf773eb475396580a9c2
mar69287 <marcoa.ruiz97@gmail.com>
2024-01-25 11:29:58 -0800

    add animation

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -1,27 +1,33 @@
 import { Box, Heading } from '@chakra-ui/react'
-import React from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
 
 const Headers = ({ left, right}) => {
   return (
-    <Box
-        w={'full'} display={'flex'} justifyContent={'center'} alignItems={'center'} gap={4}
-        mb={{base: 1, md: 7, lg: 10}}
-    >
-        <Heading
-            // size={{ base: '3xl', md: '4xl' }}
-            fontSize={{base: '45px', md: '70px'}}
-            fontWeight={'thin'} textTransform={'uppercase'}
+    <AnimatePresence>
+        <Box
+            w={'full'} display={'flex'} justifyContent={'center'} alignItems={'center'} gap={3}
+            mb={{base: 1, md: 7, lg: 10}}
         >
-            {left}
-        </Heading>
-        <Heading
-            // size={{ base: '30px', md: '4xl' }}
-            fontSize={{base: '45px', md: '70px'}}
-            fontWeight={'thin'} textTransform={'uppercase'}
-        >
-            {right}
-        </Heading>
-    </Box>
+            <Heading
+                as={motion.div}
+                fontSize={{base: '45px', md: '70px'}}
+                fontWeight={'thin'} textTransform={'uppercase'}
+                initial={{ opacity: 0, x: -10 }}
+                animate={{ opacity: 1, x: 0, transition: {delay: .4, duration: .4} }}
+            >
+                {left}
+            </Heading>
+            <Heading
+                as={motion.div}
+                fontSize={{base: '45px', md: '70px'}}
+                fontWeight={'bold'} textTransform={'uppercase'}
+                initial={{ opacity: 0, x: 10 }}
+                animate={{ opacity: 1, x: 0, transition: {delay: .4, duration: .4} }}
+            >
+                {right}
+            </Heading>
+        </Box>
+    </AnimatePresence>
   )
 }
 
