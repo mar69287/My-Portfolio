e9237746e93d8650dd5a1851a9fe6548bb339ac8
mar69287 <marcoa.ruiz97@gmail.com>
2024-01-27 16:12:00 -0800

    add playground button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,57 @@
 import { HStack, Box, Flex } from '@chakra-ui/react';
 import './Navbar.css';
 import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
 
-const Navbar = ({ setSelected, selected }) => {
+const Navbar = ({ setSelected, selected, mode, setMode }) => {
   const sectionNames = ['Home', 'About', 'Projects', 'Contact'];
   
   return (
             <HStack as={'nav'} color={'#f5f5f5'} h={{base:'3.2rem', lg: '4rem'}} fontSize={{base: 'md', md: 'lg'}} justifyContent={'space-between'} gap={2} 
                 bgColor={'rgba(0, 0, 0, 0.2)'} w={'full'} zIndex={20} position={'fixed'} left={'50%'} transform={'translateX(-50%)'} top={'0'} p={".7rem 1.7rem"}  backdropBlur={'15px'}
             >
+                {selected === 0 && <Box 
+                        pos={'absolute'} zIndex={5}
+                        left={{base:-9, sm: -9, lg: -10}} bottom={{base: -20, sm: -24, lg: -28}}
+                        color={'#fff'}
+                        fontSize={{base: '.8rem', lg: '1rem'}}
+                        as={motion.div}
+                        initial={{
+                                rotate: '270deg',
+                                opacity: 0
+                        }}
+                        animate={{
+                                opacity: 1,
+                                transition: {
+                                        delay: .3
+                                }
+                        }}
+                        display={'flex'} gap={0} justifyContent={'center'} alignItems={'center'}
+                >
+                        Playground 
+                        <Box  
+                                cursor={'pointer'}
+                                onClick={() => setMode(!mode)}
+                                border={'1px solid #fff'}
+                                w={12} ml={2} p={1}
+                                rounded={'full'}
+                                display={'flex'} pos={'relative'} alignItems={'center'}
+                                justifyContent={mode ? 'start' : 'end'}
+                                bg={mode ? '#e31b60' : '#04c2c9'}
+
+                        >
+                                <Box
+                                        h={{base: 3, lg: 4}} rounded={'full'} bg={'white'} w={{base: 3, lg: 4}}
+                                        as={motion.div}
+                                        layout
+                                        transition={{
+                                                duration: 0.75,
+                                                type: "spring",
+                                        }}                
+                                />
+                        </Box>
+                        
+                </Box>}
                 <Box
                         as={motion.div}
                         initial={{ opacity: 0 }}
@@ -16,7 +59,7 @@ const Navbar = ({ setSelected, selected }) => {
                 >
                         MR
                 </Box>
-                <Tabs selected={selected} setSelected={setSelected} sectionNames={sectionNames} />
+                {mode && <Tabs selected={selected} setSelected={setSelected} sectionNames={sectionNames} />}
             </HStack>
   )
 }
