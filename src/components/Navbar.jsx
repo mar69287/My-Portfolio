ce0b69d7e46957403b52254c36deb05533c58575
mar69287 <marcoa.ruiz97@gmail.com>
2024-01-30 14:21:15 -0800

    update design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,47 @@
-import { HStack, Box, Flex } from '@chakra-ui/react';
+import { HStack, Box, Flex, useBreakpointValue } from '@chakra-ui/react';
 import './Navbar.css';
 import { AnimatePresence, motion } from 'framer-motion';
+import { AiOutlineHome } from "react-icons/ai";
+import { IoPersonOutline } from "react-icons/io5";
+import { MdContactPhone, MdOutlineGridView } from "react-icons/md";
+import { Link } from 'react-scroll';
 
 const Navbar = ({ setSelected, selected, mode, setMode }) => {
-  const sectionNames = ['Home', 'About', 'Projects', 'Contact'];
+  const isSmallScreen = useBreakpointValue({ base: true, 'md': false });
+  const sectionHeaders = [
+        {
+                'name': 'Home',
+                'icon': AiOutlineHome
+        },
+        {
+                'name': 'Projects',
+                'icon': MdOutlineGridView
+        },
+        {
+                'name': 'About',
+                'icon': IoPersonOutline
+        },
+        {
+                'name': 'Contact',
+                'icon': MdContactPhone
+        }
+  ]
   
   return (
-            <HStack as={'nav'} color={'#f5f5f5'} h={{base:'3.2rem', lg: '4rem'}} fontSize={{base: 'md', md: 'lg'}} justifyContent={'space-between'} gap={2} 
-                bgColor={'rgba(0, 0, 0, 0.2)'} w={'full'} zIndex={20} position={'fixed'} left={'50%'} transform={'translateX(-50%)'} top={'0'} p={".7rem 1.7rem"}  backdropBlur={'15px'}
+            <HStack as={'nav'} color={'#b9b9b9'} h={{base:'3.2rem', lg: '4rem'}} fontSize={{base: 'md', md: 'lg'}} justifyContent={'space-between'} gap={2} 
+                bgColor={'rgba(0, 0, 0, 0.2)'} w={'full'} zIndex={20} position={'fixed'} left={0} right={0} top={'0'} p={".7rem 1.7rem"}  backdropBlur={'15px'}
             >
-                {selected === 0 && <Box 
+
+                <Box
+                        as={motion.div}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1, transition: {duration: .5, delay: .5} }}
+                        fontSize={{ base: 'lg', md: 'xl' }}
+                        color={'#f5f5f5'}
+                >
+                        Marco Ruiz
+                </Box>
+                {/* <Box 
                         pos={'absolute'} zIndex={5}
                         left={{base:-9, sm: -9, lg: -10}} bottom={{base: -20, sm: -24, lg: -28}}
                         color={'#fff'}
@@ -50,38 +82,32 @@ const Navbar = ({ setSelected, selected, mode, setMode }) => {
                                 />
                         </Box>
                         
-                </Box>}
-                <Box
-                        as={motion.div}
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1, transition: {duration: .5, delay: .5} }}
-                >
-                        MR
-                </Box>
-                {mode && <Tabs selected={selected} setSelected={setSelected} sectionNames={sectionNames} />}
+                </Box> */}
+                {mode && <Tabs selected={selected} setSelected={setSelected} sectionNames={sectionHeaders} isSmallScreen={isSmallScreen} />}
             </HStack>
   )
 }
 
 export default Navbar
 
-const Tabs  = ({ selected, setSelected, sectionNames }) => {
+const Tabs  = ({ selected, setSelected, sectionNames, isSmallScreen }) => {
         return (
                 <Flex
                         as={motion.div}
                         variants={boxVariant}
                         animate='visible'
-                        initial='hidden' gap={0}
+                        initial='hidden' gap={{base: 5, md: 4}}
                 >
                         <AnimatePresence>
-                                {sectionNames.map((name, index) => {
+                                {sectionNames.map((section, index) => {
                                         return (
                                                 <Tab
                                                         key={index}
                                                         setSelected={setSelected}
                                                         selected={selected === index}
-                                                        name={name}
-                                                        tabNum={index}
+                                                        name={section.name}
+                                                        Icon={section.icon}
+                                                        isSmallScreen={isSmallScreen}
                                                 />
                                         )
                                 })}
@@ -90,37 +116,62 @@ const Tabs  = ({ selected, setSelected, sectionNames }) => {
         )
 }
 
-const Tab = ({ setSelected, selected, name, tabNum}) => {
+const Tab = ({ name, Icon, isSmallScreen}) => {
+        const tabName = name === 'Home' ? 'hero' : name;
         return (
-                <Box
-                        color={'white'} pos={'relative'} w={'full'}
-                        as={motion.div}
-                        variants={listVariant}
-                >       
-                        <Box
-                                rounded={0}
-                                px={{base: '3px', sm: 1}}
-                                color={'white'} pos={'relative'} w={'full'} borderBottom={'2px solid rgba(245, 245, 245, 0.7)'}
-                                display={'flex'} justifyContent={'center'} alignItems={'center'}
-                                onClick={() => setSelected(tabNum)}
-                                cursor={selected ? 'default' : 'pointer'}
+                <Link
+                        to={tabName} spy={true} smooth={true} offset={0} duration={500}
+                >
+                        <Flex
+                                pos={'relative'} w={'full'}
+                                as={motion.div}
+                                variants={listVariant}
+                                cursor={'pointer'}
+                                fontSize={{ base: 'lg', md: 'xl', 'xl': '2xl' }}
+                                fontWeight={'thin'}
+                                
                         >
-                                <Box
-                                        color={selected ? 'white' : 'rgba(245, 245, 245, 0.7)'}
-                                        _hover={{ color: selected ? 'white' : '#e31b60', transition: 'all 0.4s ease' }}
-                                >
-                                        {name}
-                                </Box>
-                        </Box>
-                        {selected && 
-                                <Box 
-                                        as={motion.span}
-                                        layoutId="tabs-underline"
-                                        pos={'absolute'} bottom={0} left={0} right={0} zIndex={15}
-                                        h={'2px'} bg={'white'}
-                                />
-                        }
-                </Box>
+                                <WordShift word={name} Icon={Icon} isSmallScreen={isSmallScreen} />
+                        </Flex>
+                </Link>
+        )
+}
+
+const WordShift = ({ word, Icon, isSmallScreen }) => {
+        return (
+          <Box 
+            h={{base: '16px', md: '23px', lg:'23px'}} display={'inline-block'} overflow={'hidden'} lineHeight={1.1}
+            ml={1} textAlign={'left'} fontFamily={'Roboto, sans-serif'}
+            as={motion.div} whileHover={'hover'} 
+          >
+            <Box
+              as={motion.div}
+              display={'flex'} flexDirection={'column'} 
+              initial={{
+                y: "0%",
+              }}
+              variants={{
+                hover: {
+                  y: "-50%",
+                },
+              }}
+              transition={{
+                duration: 0.5,
+              }}
+            >
+                {isSmallScreen ? (
+                        <>
+                                <Box><Icon /></Box>
+                                <Box color={'#9b59b6'}><Icon /></Box>
+                        </>
+                ) : (
+                        <>
+                                <Box>{word}</Box>
+                                <Box color={'#9b59b6'}>{word}</Box>
+                        </>
+                )}
+            </Box>
+          </Box>
         )
 }
 
@@ -135,7 +186,7 @@ const boxVariant = {
                 opacity: 1,
                 transition: {
                         staggerChildren: .4,
-                        delay: .7,
+                        delay: .4,
                         when: "beforeChildren",
                 },
         },
@@ -148,3 +199,4 @@ const listVariant = {
                 opacity: 1,
         },
 }
+
