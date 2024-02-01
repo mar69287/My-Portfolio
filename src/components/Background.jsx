64ed35f5550679c96c0c3f9699648fa5ec384386
mar69287 <marcoa.ruiz97@gmail.com>
2024-02-01 10:50:39 -0800

    remove state

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,52 +1,49 @@
+import { pointsOuter } from '../three-utility';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Sphere } from '@react-three/drei';
-import { pointsOuter } from '../three-utility';
 import { useRef } from 'react';
 
-const Background = ({ selected }) => {
-
+const Background = () => {
   return (
-    <Canvas
-        camera={{
-          position: [10, -7.5, -5],
-        }}
-        style={{background: '#000', position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, height: '100%', zIndex: '0'}}
-        // style={{background: 'rgb(15, 15, 15)', height: '100vh'}}
-      >
-        <OrbitControls maxDistance={20} minDistance={10}/>
-        {/* <directionalLight /> */}
-        <PointCircle selected={selected} />
-      </Canvas>
+        <Canvas
+          camera={{
+            position: [10, -7.5, -5],
+          }}
+          style={{background: '#000', position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, height: '100%', zIndex: '0'}}        >
+          <OrbitControls maxDistance={20} minDistance={10}/>
+          <directionalLight />
+          <pointLight position={[-30, 0, -30]} power={10.0}/>
+          <PointCircle />
+        </Canvas>
   )
 }
-const PointCircle = ({ selected }) => {
-    const ref = useRef(null)
-    useFrame(({ clock }) => {
-      if (ref.current?.rotation) {
-        ref.current.rotation.z = clock.getElapsedTime() * 0.05;
-      }
-    });
-    return (
-      <group ref={ref}>
-          {pointsOuter.map((point) => (
-            <Point key={point.idx} position={point.position} color={point.color} selected={selected} />
-          ))}
-      </ group>
-    )
-  }
-  
-  const Point = ({ position, color, selected }) => {
-    
-    return (
-      <Sphere position={position} args={[0.03, 10, 10]}>
-        <meshStandardMaterial
-          emissive={color}
-          emissiveIntensity={selected ? 0.7 : 0.3}
-          roughness={0.5}
-          color={color}
-        />
-      </Sphere>
-    );
-  };
+const PointCircle = () => {
+  const ref = useRef(null)
+  useFrame(({ clock }) => {
+    if (ref.current?.rotation) {
+      ref.current.rotation.z = clock.getElapsedTime() * 0.05;
+    }
+  });
+  return (
+    <group ref={ref}>
+        {pointsOuter.map((point) => (
+          <Point key={point.idx} position={point.position} color={point.color} />
+        ))}
+    </ group>
+  )
+}
+
+const Point = ({ position, color }) => {
+  return (
+    <Sphere position={position} args={[0.03, 10, 10]}>
+      <meshStandardMaterial
+        emissive={color}
+        emissiveIntensity={0.9}
+        roughness={0.5}
+        color={color}
+      />
+    </Sphere>
+  );
+};
 
 export default Background
